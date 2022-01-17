a126a68809076d4acd35de177a7f14235bd88286
Sakshi Sutar <53054910+Sakshi-Sutar@users.noreply.github.com>
2022-01-17 16:31:12 +0530

    Update MathClass.java

--- MathClass.java
+++ MathClass.java
@@ -1,7 +1,3 @@
-//15-5-20; friday
-//8:50 pm
-//Math is a class in java which has different methods to perform mathematical tasks
-
 public class MathClass
 {
 	public static void main(String[] args)
@@ -12,16 +8,13 @@ public class MathClass
 		System.out.println(Math.abs(-7));
 
 		System.out.println(Math.random());				//returns random numbers between 0.0 to 1.0(exclusive)
-		System.out.println((int)(Math.random()*101));	//returns random numbers between 0 to 100(inclusive)
+		System.out.println((int)(Math.random()*101));			//returns random numbers between 0 to 100(inclusive)
 	}
 }
 
 
 /* OUTPUT
 
-C:\Users\Sakshi Sutar\codes\java>javac MathClass.java
-
-C:\Users\Sakshi Sutar\codes\java>java MathClass
 5
 4
 8.0
@@ -29,14 +22,13 @@ C:\Users\Sakshi Sutar\codes\java>java MathClass
 0.8610689647588134
 66
 
-C:\Users\Sakshi Sutar\codes\java>java MathClass
+
+
 5
 4
 8.0
 7
 0.6965672643899194
 100
-
-C:\Users\Sakshi Sutar\codes\java>
 
 */
