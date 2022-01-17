e9061b6d40f6709827dca6be1aa40fb8895eaa4b
Sakshi Sutar <53054910+Sakshi-Sutar@users.noreply.github.com>
2022-01-17 16:20:16 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World !!");
+	}
+}
+
+
+/* OUTPUT
+
+C:\Users\Sakshi Sutar\codes\java>javac Hello.java
+
+C:\Users\Sakshi Sutar\codes\java>java Hello
+Hello World !!
+
+C:\Users\Sakshi Sutar\codes\java>
+
+*/
