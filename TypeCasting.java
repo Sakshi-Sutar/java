f673d59e55a495e83b709dbdbc4c62e1339db994
Sakshi Sutar <53054910+Sakshi-Sutar@users.noreply.github.com>
2022-01-17 16:41:14 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,28 @@
+public class TypeCasting
+{
+	public static void main(String[] args)
+	{
+		int x=5;
+		float y=x;					//widening casting - automatic conversion from int to float
+
+		System.out.println(x);
+		System.out.println(y);
+
+		float p=6.7f;
+		int q=(int) p;				//narrowing casting - manual conversion needed
+
+		System.out.println(p);
+		System.out.println(q);
+	}
+}
+
+
+
+/* OUTPUT
+
+5
+5.0
+6.7
+6
+
+*/
